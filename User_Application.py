83de5948cb5485386a571748f6c1c1a4e718fdc6
Aslam-me <simanto.me@gmail.com>
2022-02-22 00:10:12 +0100

    IP Address and Port number added to welcome string

--- User_Application.py
+++ User_Application.py
@@ -7,7 +7,7 @@ UDP_PORT = 16000
 def print_in_Dot_Matrix_Display(string):
     
     with open("/dev/DotMatrixDisplay", "w") as Dot_Matrix_Display:
-        Dot_Matrix_Display.write(string)
+        Dot_Matrix_Display.write(string+"    ")
 
 
 if __name__ == "__main__":
@@ -16,10 +16,12 @@ if __name__ == "__main__":
     sock.bind((UDP_IP, UDP_PORT))
 
     print("UDP Server Started")
-    print_in_Dot_Matrix_Display("UDP Server Started    ")
+    print_in_Dot_Matrix_Display("UDP Server Started at IP:"+str(UDP_IP) + ",Port:"+ str(UDP_PORT))
     
 
     while True:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
         print("received message: %s" % data.decode("utf-8"))
         print_in_Dot_Matrix_Display(data.decode("utf-8"))
+        
+    sock.close()
